c85b967fc042e039c1825c03df0b479186741955
Sonish Maharjan <sonishmzn95@gmail.com>
2021-03-04 20:46:44 +0545

    auth in user removed

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ const postRoutes = require("./routes/postRoutes");
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use("/users", authenticate, userRoutes);
+router.use("/users", userRoutes);
 router.use("/post", postRoutes);
 
 module.exports = router;
